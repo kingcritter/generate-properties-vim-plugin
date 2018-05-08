4697fc271b1d2f47a9506c1c2258f031254cd212
kingcritter <kingcritter@gmail.com>
2018-05-07 21:14:39 -0400

    Object<type> works properly now :scream:

--- python/generate_properties.py
+++ python/generate_properties.py
@@ -4,38 +4,43 @@ try:
 except: 
     pass
 
-template = Template("""private ${_primitive}Property${_obj_type} ${_name} = new Simple${_primitive}Property${_brackets}(this, "${_name}");
-public ${_primitive}Property${_obj_type} ${_name}Property() {return this.${_name};}
-public ${_get_and_set_type}${_obj_type} get${_big_name}() {return this.${_name}.get();}
-public void set${_big_name}(${_get_and_set_type}${_obj_type} ${_name}) {this.${_name}.set(${_name});}""")
+template = Template("""private ${primitive}Property${generic} ${name} = new Simple${primitive}Property${brackets}(this, "${name}");
+public ${primitive}Property${generic} ${name}Property() {return this.${name};}
+public ${get_and_set_type} get${big_name}() {return this.${name}.get();}
+public void set${big_name}(${get_and_set_type} ${name}) {this.${name}.set(${name});}""")
 
 
-def generate_property(primitive, name, obj_type=""):
+def generate_property(primitive, name, obj_type):
     get_and_set_type = primitive
     brackets = ""
-    
-    if obj_type:
-        obj_type = "<" + obj_type + ">"
-        primitive_or_type = obj_type
-        brackets = "<>"
+    generic = ""
 
+    if obj_type:
+        brackets = "<>"
+        generic = "<{}>".format(obj_type)
     if primitive in ["List", "Map", "Set"]:
-        get_and_set_type = "Observable" + primitive
+        brackets = "<>"
+        get_and_set_type = "Observable{}{}".format(primitive, generic)
+
+    elif primitive == "Object":
+        get_and_set_type = obj_type
+    else:
+        obj_type = ""
 
     big_name = name[0].upper() + name[1:]
 
-    output = template.substitute(_primitive=primitive,
-                                 _name=name,
-                                 _big_name=big_name,
-                                 _obj_type=obj_type,
-                                 _get_and_set_type=get_and_set_type,
-                                 _brackets=brackets)
+    output = template.substitute(primitive=primitive,
+                                 name=name,
+                                 big_name=big_name,
+                                 generic=generic,
+                                 get_and_set_type=get_and_set_type,
+                                 brackets=brackets)
 
     return output
 
 def gen_prop(args):
     if len(args) not in [2, 3]:
-        print("Not enough arguments!")
+        print("Usage: :GenProp PropertyType name [ElementType]")
         return
     
     primitive = args[0]
